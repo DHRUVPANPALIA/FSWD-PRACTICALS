d66f868fcf7738c129ce4a8ba7f6209607be9d71
Your Name <you@example.com>
2023-07-24 12:32:40 +0530

    Clone-Folder

--- reactnavbar/src/App.js
+++ reactnavbar/src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    
-        <Navbar />
+    <Router>
+      <Navbar />
+    </Router>
+        
     
   );
 }
--- reactnavbar/src/components/Navbar/NavbarElements.js
+++ reactnavbar/src/components/Navbar/NavbarElements.js
@@ -64,6 +64,9 @@ export const NavBtn = styled.nav`
 
 export const NavBtnLink = styled.(Link)`
     border-radius: 4px;
-    background: ;
+    background: #256ce1;
+    padding: 10px 22px;
+    
+
 `
 
